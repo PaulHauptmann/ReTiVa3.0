1eec164c99e8ac72688986aebe9872e37c550c78
tomwolf80 <118889398+tomwolf80@users.noreply.github.com>
2023-01-09 14:58:04 +0100

    gui erste entwürfe, aufsetzen der umgebung

---
+++ openEAR-0.1.0 Kopie/GUI/CustomTkinter/gui_start_v1.py
@@ -0,0 +1,9 @@
+import customtkinter
+
+#Color-Theme Einstellungen aus eigener Datei übernehmen (Custom CI, muss nur in der .json Datei verändert werden)
+customtkinter.set_default_color_theme("openEAR-0.1.0 Kopie/GUI/CustomTkinter/retiva_dark-blue.json")
+
+#Mode auf Systemstandard festlegen
+customtkinter.set_appearance_mode("system")
+
+
