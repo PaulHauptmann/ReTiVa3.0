b219db7ef7e948ffc8fb49cc67bd12f1a13bd35f
PaulHauptmann <paulhauptmann20@gmail.com>
2023-01-11 13:59:36 +0100

    commit

--- openEAR-0.1.0 Kopie/TestDataExtractor2.py
+++ openEAR-0.1.0 Kopie/TestDataExtractor2.py
@@ -36,9 +36,9 @@ class EMO:
 class Main:
 
     #path definitionen
-    file_path = '/Users/paul/Documents/GitHub/ReTiVa3.0/openEAR-0.1.0 Kopie/smile.log'
-    directory_path = '/Users/paul/Documents/GitHub/ReTiVa3.0/openEAR-0.1.0 Kopie'
-    archive_path = '/Users/paul/Documents/GitHub/ReTiVa3.0/openEAR-0.1.0 Kopie/SmileArchiv'
+    file_path = 'openEAR-0.1.0 Kopie/smile.log'
+    directory_path = 'openEAR-0.1.0 Kopie/'
+    archive_path = 'openEAR-0.1.0 Kopie/SmileArchiv'
 
     #listen Initialisierung
     DataSpeakRatio = [0]
