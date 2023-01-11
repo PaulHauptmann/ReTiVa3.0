35fc935c8597202405db4fd8b468a212df320c89
tomwolf80 <118889398+tomwolf80@users.noreply.github.com>
2023-01-11 13:58:56 +0100

    push

--- openEAR-0.1.0 Kopie/mainGUI2.py
+++ openEAR-0.1.0 Kopie/mainGUI2.py
@@ -1,16 +1,15 @@
 import tkinter as tk
 import customtkinter
 from time import *
-import TestReader3 as T3
+import TestDataExtractor2 as T3
 import time
 import threading
 
-window = tk.Tk()
 
 
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-#customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 #Color-Theme Einstellungen aus eigener Datei übernehmen (Custom CI, muss nur in der .json Datei verändert werden)
 #customtkinter.set_default_color_theme('/Users/paul/Documents/GitHub/ReTiVa3.0/openEAR-0.1.0 Kopie/retiva_dark-blue.json')
@@ -27,7 +26,7 @@ class App(customtkinter.CTk):
 
 
         # configure window
-        self.title("CustomTkinter complex_example.py")
+        self.title("ReTiVA")
         self.geometry(f"{1100}x{580}")
 
         
@@ -44,13 +43,13 @@ class App(customtkinter.CTk):
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="ReTiVA", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Start" , command=self.button_starter)
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Neue Analyse")
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.button_stop_command)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Start", command=self.button_starter)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Stop", command=self.button_stop_command)
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
@@ -63,49 +62,17 @@ class App(customtkinter.CTk):
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
-        # create main entry and button
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
-        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
-
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
-        self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
-
+        
         # create textbox
         self.textbox = customtkinter.CTkTextbox(self, width=250)
         self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
-        # create tabview
-        self.tabview = customtkinter.CTkTabview(self, width=250)
-        #self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.tabview.add("CTkTabview")
-        self.tabview.add("Tab 2")
-        self.tabview.add("Tab 3")
-        self.tabview.tab("CTkTabview").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
-        self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
-
-        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("CTkTabview"), dynamic_resizing=False,
-                                                        values=["Value 1", "Value 2", "Value Long Long Long"])
-        self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("CTkTabview"),
-                                                    values=["Value 1", "Value 2", "Value Long....."])
-        self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
-        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("CTkTabview"), text="Open CTkInputDialog",
-                                                           command=self.open_input_dialog_event)
-        self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
-        self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("Tab 2"), text="CTkLabel on Tab 2")
-        self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
-
         
-        
-
         
         # set default values
-        self.sidebar_button_3.configure(state="disabled", text="Disabled CTkButton")
-        
-        self.appearance_mode_optionemenu.set("Dark")
+        self.sidebar_button_1.configure(state = "disabled")
+        self.appearance_mode_optionemenu.set("System")
         self.scaling_optionemenu.set("100%")
-        self.optionmenu_1.set("CTkOptionmenu")
-        self.combobox_1.set("CTkComboBox")
         
         
         
@@ -121,41 +88,37 @@ class App(customtkinter.CTk):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
 
-    def sidebar_button_event(self):
-        #self.textbox.insert("0.0", str(T3.update()))
-        print("check")
-        #time.sleep(1)
-        window.after(1000, self.sidebar_button_event)
-
-
+    
+    
 
     
    
-
-    stop = False
 
     def button_stop_command(self):
         # If the STOP button is pressed then terminate the loop
         global stop
         stop = True
 
-    def button_start_command(self):
+    def start_command(self):
         global stop
         stop = False
-        j = 1
         while True and not stop:
         
-        
-            self.textbox.insert("0.0", T3.update())
+            #Textbox vollständig löschen
+            #self.textbox.delete("0.0", tk.END)
+
+            #Neuen Text aus T3 einfügen
+            #self.textbox.insert("0.0", T3.update())
+            self.textbox.insert("0.0", "Test")
+
+            time.sleep(0.5)
             
-            time.sleep(0.1)
-            j = j+1
 
 
 
     def button_starter(self):
 
-        t = threading.Thread(target=self.button_start_command)
+        t = threading.Thread(target=self.start_command)
         t.start()
 
 
