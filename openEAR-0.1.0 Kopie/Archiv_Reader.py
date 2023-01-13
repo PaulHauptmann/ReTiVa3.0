497f2a9de6d1e04338ba007fd7cd462ed23618dd
tomwolf80 <118889398+tomwolf80@users.noreply.github.com>
2023-01-13 10:12:23 +0100

    changed path

--- openEAR-0.1.0 Kopie/Archiv_Reader.py
+++ openEAR-0.1.0 Kopie/Archiv_Reader.py
@@ -3,7 +3,7 @@
 class Main:
 
     Filename = None
-    Archive_path = '/Users/paul/Documents/GitHub/ReTiVa3.0/openEAR-0.1.0 Kopie/SmileArchiv'
+    Archive_path = 'openEAR-0.1.0 Kopie/SmileArchiv'
 
     def Set_Filename(filename):
         Main.Filename = filename
