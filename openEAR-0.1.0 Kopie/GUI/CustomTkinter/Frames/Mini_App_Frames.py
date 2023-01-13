bb3ea06b7d4194c6fcaee8f5a31a302e0ad98dbf
tomwolf80 <118889398+tomwolf80@users.noreply.github.com>
2023-01-13 13:06:18 +0100

    bastel

--- openEAR-0.1.0 Kopie/GUI/CustomTkinter/Frames/Mini_App_Frames.py
+++ openEAR-0.1.0 Kopie/GUI/CustomTkinter/Frames/Mini_App_Frames.py
@@ -1,15 +1,25 @@
 import tkinter
 import customtkinter
+from tkinter import ttk
+import time
 
 class ScoreFrame(customtkinter.CTkFrame):
-    def __init__(self, master = None):
+    def __init__(self, master = None, is_blinking = False):
         super().__init__(master)
 
-        self.score_title=customtkinter.CTkLabel(self, text="Live-Score:")
+        
+        self.score_title=tkinter.Label(self, text="Live-Score:")
+        #self.score_title=customtkinter.CTkLabel(self, text="Live-Score:")
         self.score_title.grid(row = 0, column = 0)
 
         self.score_label = customtkinter.CTkLabel(self, width=100, height=100,corner_radius=20, fg_color="green", text="")
         self.score_label.grid(row = 1, column = 0)
+        
+
+    def set_pause_event(self, is_pause):
+        self.pause = is_pause
+
+        
 
 
 
