8ba30c44bf85f3b19a58ad7104c39d4c68918380
PaulHauptmann <paulhauptmann20@gmail.com>
2023-01-13 10:46:23 +0100

    Archive_Reader fertig, TDE verbessert

--- openEAR-0.1.0 Kopie/Test2.py
+++ openEAR-0.1.0 Kopie/Test2.py
@@ -1,3 +1,9 @@
-import Test1
+import Test1 as T1
 
-print(Test1.TestClass.testvariable1)
+data = T1.Get_Data("/Users/paul/Desktop/SmileArchiv/2023-01-10 13:11:33.xlsx")
+archive_dt_string = data.get("Archive_dt_string", "Key not found")
+
+print (archive_dt_string)
+
+
+
