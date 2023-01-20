4c88a53c1b13a3190d9bafffe3464b528f7be36c
PaulHauptmann <paulhauptmann20@gmail.com>
2023-01-20 14:57:46 +0100

    zum mergen

--- openEAR-0.1.0 Kopie/GUI/CustomTkinter/CustomObjects.py
+++
@@ -1,9 +0,0 @@
-
-
-
-## Objekt, das die Start-Einstellungen speichert, die im New_Analysis_Window festgelegt werden ##
-
-class Startupsettings(object):
-    selected_audio_device = ""
-    session_name = ""
-    working_mode= ""
