d09feb4331edc1e7fa45f9c951868bc9318f27a4
PaulHauptmann <paulhauptmann20@gmail.com>
2023-01-26 15:59:15 +0100

    pus

--- openEAR-0.1.0 Kopie/Starter.py
+++ openEAR-0.1.0 Kopie/Starter.py
@@ -2,9 +2,15 @@ import subprocess
 from tkinter import *
 
 def run_smilextract():
-    subprocess.run(["SMILExtract", "-C", "config/emobase_live4.conf"], cwd="/Users/paul/Documents/GitHub/ReTiVa3.0/openEAR-0.1.0 Kopie/")
+    global process
+    process = subprocess.Popen(["SMILExtract", "-C", "config/emobase_live4.conf"], cwd="/Users/paul/Documents/GitHub/ReTiVa3.0/openEAR-0.1.0 Kopie/")
+
+def stop_smilextract():
+    process.kill()
 
 root = Tk()
-button = Button(root, text="Run SMILExtract", command=run_smilextract)
-button.pack()
+start_button = Button(root, text="Start SMILExtract", command=run_smilextract)
+stop_button = Button(root, text="Stop SMILExtract", command=stop_smilextract)
+start_button.pack()
+stop_button.pack()
 root.mainloop()
